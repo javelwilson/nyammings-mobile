c257aaf034f44a807cca07031c630bb67f10ea0c
Javel Wilson <javelawilson@gmail.com>
2020-10-30 21:02:50 -0500

    Initialize application

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
